01bfda7febd1c9c86eccdc4be2c3a34c99d66594
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2019-04-25 13:24:20 +0200

    Add basic layout structure.

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
+require './presenters/home'
 
 
 get '/' do
+  @presenter = Presenters::Home.new(
+    title: 'Nexmo Developer',
+    env: Sinatra::Application.environment,
+  )
   erb 'Hello World'
 end
