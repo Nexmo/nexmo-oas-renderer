ed459b9e6078f3edd8ca345d8f5db650d5012a1f
Michael Heap <m@michaelheap.com>
2020-10-23 20:25:42 +0100

    Render JSON examples if they exist

--- lib/nexmo/oas/renderer/presenters/response_tab/panel.rb
+++ lib/nexmo/oas/renderer/presenters/response_tab/panel.rb
@@ -25,6 +25,8 @@ module Nexmo
 
             def content
               if @content == :responses
+                return generate_examples(@schema['examples']) if @schema['examples'] && @format == 'application/json'
+
                 Nexmo::OAS::Renderer::ResponseParserDecorator
                   .new(@schema)
                   .html(@format, xml_options: @schema['xml'], theme_light: @theme_light)
@@ -36,6 +38,28 @@ module Nexmo
                   endpoint: @endpoint,
                 }]
               end
+            end
+
+            def generate_examples(examples)
+              switcher = Nexmo::OAS::Renderer::Presenters::ContentSwitcher.new(label: "Example", format: @format, force_type: 'dropdown', theme_light: @theme_light)
+
+              examples.each_with_index do |v,k|
+              response = JSON.neat_generate(v[1], {
+                  wrap: true,
+                  after_colon: 1,
+              })
+              content = <<~HEREDOC
+                <pre class="pre-wrap language-json #{@theme_light ? 'Vlt-prism--dark' : ''} Vlt-prism--copy-disabled"><code>#{response}</code></pre>
+              HEREDOC
+                 switcher.add_content(
+                    title: v[0].titleize,
+                    content: content,
+                    tab_id: v[0],
+                    active: k == 0
+                )
+              end
+
+              switcher.render
             end
           end
         end
